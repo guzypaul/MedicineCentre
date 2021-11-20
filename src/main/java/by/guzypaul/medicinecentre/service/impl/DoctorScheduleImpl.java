81034b8863f73f9aace018381b609886a529f3aa
Paul <guzypaul@gmail.com>
2021-11-20 16:37:34 +0300

    Doctor service update

---
+++ src/main/java/by/guzypaul/medicinecentre/service/impl/DoctorScheduleImpl.java
@@ -0,0 +1,75 @@
+package by.guzypaul.medicinecentre.service.impl;
+
+import by.guzypaul.medicinecentre.dao.exception.DaoException;
+import by.guzypaul.medicinecentre.dao.interfaces.DoctorScheduleDao;
+import by.guzypaul.medicinecentre.entity.DoctorSchedule;
+import by.guzypaul.medicinecentre.service.exception.ServiceException;
+import by.guzypaul.medicinecentre.service.interfaces.DoctorScheduleService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class DoctorScheduleImpl implements DoctorScheduleService {
+    private static final Logger logger = LogManager.getLogger();
+    private final DoctorScheduleDao doctorScheduleDao;
+
+    public DoctorScheduleImpl(DoctorScheduleDao doctorScheduleDao) {
+        this.doctorScheduleDao = doctorScheduleDao;
+    }
+
+    @Override
+    public List<DoctorSchedule> readAll() throws ServiceException {
+        logger.log(Level.DEBUG, "readAll");
+        try {
+            List<DoctorSchedule> doctorSchedules;
+            doctorSchedules = doctorScheduleDao.readAll();
+            return doctorSchedules;
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public DoctorSchedule readById(Integer id) throws ServiceException {
+        logger.log(Level.DEBUG, "readById(), DoctorSchedule id:" + id);
+        try {
+            DoctorSchedule doctorSchedule;
+            doctorSchedule = doctorScheduleDao.readById(id);
+            return doctorSchedule;
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean deleteById(Integer id) throws ServiceException {
+        logger.log(Level.DEBUG, "delete DoctorSchedule id:" + id);
+        try {
+            return doctorScheduleDao.deleteById(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean create(DoctorSchedule entity) throws ServiceException {
+        logger.log(Level.DEBUG, "create " + entity);
+        try {
+            return doctorScheduleDao.create(entity);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean update(DoctorSchedule entity) throws ServiceException {
+        logger.log(Level.DEBUG, "update " + entity);
+        try {
+            return doctorScheduleDao.update(entity);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+}
---
+++ src/main/java/by/guzypaul/medicinecentre/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,75 @@
+package by.guzypaul.medicinecentre.service.impl;
+
+import by.guzypaul.medicinecentre.dao.exception.DaoException;
+import by.guzypaul.medicinecentre.dao.interfaces.DoctorDao;
+import by.guzypaul.medicinecentre.entity.Doctor;
+import by.guzypaul.medicinecentre.service.exception.ServiceException;
+import by.guzypaul.medicinecentre.service.interfaces.DoctorService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class DoctorServiceImpl implements DoctorService {
+    private static final Logger logger = LogManager.getLogger();
+    private final DoctorDao doctorDao;
+
+    public DoctorServiceImpl(DoctorDao doctorDao) {
+        this.doctorDao = doctorDao;
+    }
+
+    @Override
+    public List<Doctor> readAll() throws ServiceException {
+        logger.log(Level.DEBUG, "readAll");
+        try {
+            List<Doctor> doctors;
+            doctors = doctorDao.readAll();
+            return doctors;
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public Doctor readById(Integer id) throws ServiceException {
+        logger.log(Level.DEBUG, "readById(), Doctor id:" + id);
+        try {
+            Doctor doctor;
+            doctor = doctorDao.readById(id);
+            return doctor;
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean deleteById(Integer id) throws ServiceException {
+        logger.log(Level.DEBUG, "delete Doctor id:" + id);
+        try {
+            return doctorDao.deleteById(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean create(Doctor entity) throws ServiceException {
+        logger.log(Level.DEBUG, "create " + entity);
+        try {
+            return doctorDao.create(entity);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean update(Doctor entity) throws ServiceException {
+        logger.log(Level.DEBUG, "update " + entity);
+        try {
+            return doctorDao.update(entity);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+}
