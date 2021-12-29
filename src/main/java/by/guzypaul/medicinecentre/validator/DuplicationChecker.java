b19a4e18f187d8903fda70466acbdb82dcc14834
Paul <guzypaul@gmail.com>
2021-12-29 22:54:23 +0300

    DuplicationChecker added

---
+++ src/main/java/by/guzypaul/medicinecentre/validator/DuplicationChecker.java
@@ -0,0 +1,19 @@
+package by.guzypaul.medicinecentre.validator;
+
+import by.guzypaul.medicinecentre.dao.DaoException;
+import by.guzypaul.medicinecentre.dao.DaoFactory;
+import by.guzypaul.medicinecentre.dao.interfaces.UserDao;
+import by.guzypaul.medicinecentre.entity.User;
+
+public class DuplicationChecker {
+    private final UserDao userDao;
+
+    public DuplicationChecker() {
+        userDao = DaoFactory.getInstance().getUserDao();
+    }
+
+    public boolean checkDuplication(User user) throws DaoException { //todo doubleCheck it
+        return !userDao.readByEmail(user.getEmail()).isPresent()
+                && !userDao.readByPhone(user.getPhone()).isPresent();
+    }
+}
