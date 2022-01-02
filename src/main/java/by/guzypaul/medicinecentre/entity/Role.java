b59d1004eb4d0677fbe6031849ee2f0987582264
Paul <guzypaul@gmail.com>
2022-01-02 14:58:51 +0300

    fixed

--- src/main/java/by/guzypaul/medicinecentre/entity/Role.java
+++ src/main/java/by/guzypaul/medicinecentre/entity/Role.java
@@ -1,17 +1,15 @@
 package by.guzypaul.medicinecentre.entity;
 
-import java.util.Arrays;
-
 public enum Role {
     ADMIN,
     MODERATOR,
     USER,
     DOCTOR;
 
-    public static Role findRole(String roleName) {
+    /*public static Role findRole(String roleName) {
         return Arrays.stream(Role.values())
                 .filter(currentRole -> currentRole.equals(roleName.toUpperCase()))
                 .findFirst()
                 .orElse(USER); // todo USER???
-    }
+    }*/
 }
