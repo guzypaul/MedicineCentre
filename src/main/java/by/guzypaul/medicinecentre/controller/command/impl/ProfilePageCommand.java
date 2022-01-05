bb1e5494426bc9897dd6c4a06c0759d9d4c8e6eb
Paul <guzypaul@gmail.com>
2022-01-05 21:32:30 +0300

    profile_page updated

--- src/main/java/by/guzypaul/medicinecentre/controller/command/impl/ProfilePageCommand.java
+++ src/main/java/by/guzypaul/medicinecentre/controller/command/impl/ProfilePageCommand.java
@@ -3,12 +3,34 @@ package by.guzypaul.medicinecentre.controller.command.impl;
 import by.guzypaul.medicinecentre.controller.command.Command;
 import by.guzypaul.medicinecentre.controller.command.CommandException;
 import by.guzypaul.medicinecentre.controller.command.Router;
+import by.guzypaul.medicinecentre.entity.User;
+import by.guzypaul.medicinecentre.service.ServiceFactory;
+import by.guzypaul.medicinecentre.service.exception.ServiceException;
+import by.guzypaul.medicinecentre.service.interfaces.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 public class ProfilePageCommand implements Command {
+    private final UserService userService;
+
+    public ProfilePageCommand() {
+        userService = ServiceFactory.getInstance().getUserService();
+    }
+
     @Override
     public Router execute(HttpServletRequest request) throws CommandException {
-        return new Router("/jsp/profile_page.jsp", Router.Type.FORWARD);
+        try {
+            String userId = request.getSession().getAttribute("userId").toString();
+            Optional<User> userOptional = userService.readById(userId);
+            if(userOptional.isPresent()){
+                request.setAttribute("user", userOptional.get());
+                return new Router("/jsp/profile_page.jsp", Router.Type.FORWARD);
+            }
+
+            throw new CommandException("Invalid user");
+        } catch (ServiceException e) {
+            throw new CommandException(e);
+        }
     }
 }
--- src/main/webapp/jsp/profile_page.jsp
+++ src/main/webapp/jsp/profile_page.jsp
@@ -9,10 +9,15 @@
     <title>Profile</title>
     <c:import url="header.jsp"/>
     <div class="container-fluid">
-        <h1>WELCOME ${role} in your PERSONAL AREA!</h1>
-        <div>
-            <h3>My appointments:</h3>
-
+        <h1>WELCOME <c:out value="${user.name} ${user.surname}"/> in your PERSONAL AREA with ${role} rights !</h1>
+        <br>
+        <div class="row">
+            <div class="col-lg-8">
+                <h2>Your personal information</h2>
+                <br>
+                <h4>Email: <c:out value="${user.email}"/></h4><br/>
+                <h4>Phone number: <c:out value="${user.phone}"/></h4><br/>
+            </div>
         </div>
     </div>
 <c:import url="footer.jsp"/>
