39fc73e95582bb2a93ab6e3c68e8d60b75d4c130
Paul <guzypaul@gmail.com>
2022-02-03 11:06:50 +0300

    UserServiceTest was added

--- src/test/java/by/guzypaul/medicinesentre/service/impl/UserServiceTest.java
+++ src/test/java/by/guzypaul/medicinesentre/service/impl/UserServiceTest.java
@@ -1,4 +1,112 @@
 package by.guzypaul.medicinesentre.service.impl;
 
+import by.guzypaul.medicinecentre.dao.DaoException;
+import by.guzypaul.medicinecentre.dao.interfaces.UserDao;
+import by.guzypaul.medicinecentre.entity.Role;
+import by.guzypaul.medicinecentre.entity.User;
+import by.guzypaul.medicinecentre.service.ServiceFactory;
+import by.guzypaul.medicinecentre.service.checker.UserCreatingDuplicationChecker;
+import by.guzypaul.medicinecentre.service.checker.UserUpdatingDuplicationChecker;
+import by.guzypaul.medicinecentre.service.exception.ServiceException;
+import by.guzypaul.medicinecentre.service.interfaces.UserService;
+import by.guzypaul.medicinecentre.service.validator.UserValidator;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class UserServiceTest {
+    private UserService userService;
+    private UserDao userDao;
+    private User firstTestUser;
+    private User secondTestUser;
+    private UserCreatingDuplicationChecker userCreatingDuplicationChecker;
+    private UserUpdatingDuplicationChecker userUpdatingDuplicationChecker;
+    private UserValidator userValidator;
+
+    @BeforeEach
+    void setUp() {
+        userService = ServiceFactory.getInstance().getUserService();
+        userDao = mock(UserDao.class);
+        userValidator = mock(UserValidator.class);
+        userCreatingDuplicationChecker = mock(UserCreatingDuplicationChecker.class);
+        userUpdatingDuplicationChecker = mock(UserUpdatingDuplicationChecker.class);
+
+        firstTestUser = new User(1, "Dmitriy", "Kruglov", "kruglov@gmail.com",
+                "123456", "+37525123456", Role.USER);
+        secondTestUser = new User(2, "Vladislav", "Romanovskiy", "romavlad@gmail.com",
+                "12345678", "+37525123666", Role.USER);
+    }
+
+    @Test
+    void readAllUsersTest() throws DaoException, ServiceException {
+        List<User> userList = Arrays.asList(firstTestUser, secondTestUser);
+        when(userDao.readAll()).thenReturn(userList);
+        Assert.assertEquals(userList, userService.readAll());
+    }
+
+    @Test
+    void readUsersByIdTest() throws DaoException, ServiceException {
+        when(userDao.readById(1)).thenReturn(Optional.of(firstTestUser));
+        Assert.assertEquals(firstTestUser, userService.readById("1"));
+    }
+
+    @Test
+    void readUserWithInvalidIdTest() {
+        Assert.assertThrows(ServiceException.class, () -> userService.readById("egeg"));
+    }
+
+    @Test
+    void addUserTest() throws DaoException, ServiceException {
+        when(userDao.create(firstTestUser)).thenReturn(true);
+        when(userValidator.validateUser(firstTestUser)).thenReturn(true);
+        when(userCreatingDuplicationChecker.checkDuplication(firstTestUser)).thenReturn(true);
+        Assert.assertTrue(userService.create(firstTestUser));
+    }
+
+    @Test
+    void deleteUserByIdTest() throws DaoException, ServiceException {
+        when(userDao.deleteById(1)).thenReturn(true);
+        Assert.assertTrue(userService.deleteById("1"));
+    }
+
+    @Test
+    void deleteUserWithInvalidIdTest() {
+        Assert.assertThrows(ServiceException.class, () -> userService.deleteById("dada"));
+    }
+
+    @Test
+    void updateUserTest() throws DaoException, ServiceException {
+        when(userValidator.validateUserForUpdating(firstTestUser)).thenReturn(true);
+        when(userUpdatingDuplicationChecker.checkDuplication(firstTestUser)).thenReturn(true);
+        when(userDao.update(firstTestUser)).thenReturn(true);
+        Assert.assertTrue(userService.update(firstTestUser));
+    }
+
+    @Test
+    void updateUserWithInvalidIdTest() throws DaoException {
+        firstTestUser.setId(0);
+        when(userValidator.validateUserForUpdating(firstTestUser)).thenReturn(true);
+        when(userUpdatingDuplicationChecker.checkDuplication(firstTestUser)).thenReturn(true);
+        when(userDao.update(firstTestUser)).thenReturn(true);
+        Assert.assertThrows(ServiceException.class, () -> userService.update(firstTestUser));
+    }
+
+    @Test
+    void readUserByEmailTest() throws DaoException, ServiceException {
+        when(userDao.readByEmail("kruglov@gmail.com")).thenReturn(Optional.of(firstTestUser));
+        Assert.assertEquals(Optional.of(firstTestUser), userService.readByEmail("kruglov@gmail.com"));
+    }
+
+    @Test
+    void readUserByPhoneTest() throws DaoException, ServiceException {
+        when(userDao.readByPhone("+37525123456")).thenReturn(Optional.of(firstTestUser));
+        Assert.assertEquals(Optional.of(firstTestUser), userService.readByPhone("+37525123456"));
+    }
 }
